f7ee4f5d9636f84015616e13d84b90ee822bb1ea
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-08-18 00:34:17 -0700

    Add moves list into state

---
+++ src/state/moves.js
@@ -0,0 +1,18 @@
+export default {
+  specialStomp: {
+    name: 'Special Stomp',
+    damage: 10,
+  },
+  thunderKick: {
+    name: 'Thunder Kick',
+    damage: 20,
+  },
+  crush: {
+    name: 'Crush',
+    damage: 10,
+  },
+  lick: {
+    name: 'Lick',
+    damage: 10,
+  },
+};
