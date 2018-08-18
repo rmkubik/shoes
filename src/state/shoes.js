336a85de9545f9a4c93e458f473db386244517c6
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-08-18 01:00:13 -0700

    Add moves to shoe state

--- src/state/shoes.js
+++ src/state/shoes.js
@@ -1,3 +1,5 @@
+import moves from './moves';
+
 export default {
   boot: {
     name: 'Boot',
@@ -14,5 +16,6 @@ export default {
       current: 100,
     },
     image: 'https://r-k.io/assets/images/ssj18parody/shoes_1.png',
+    moves: [moves.thunderKick, moves.specialStomp, moves.lick, moves.crush],
   },
 };
