b68658f34cc975c3e6ab34101afd14cbd60e5ad8
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-08-17 22:10:25 -0700

    Messy first pass at moving callback from codepen

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,19 @@
 import { h } from 'hyperapp';
 
+const attack = () => {
+  const img = document.querySelector('#i2');
+  img.addEventListener('animationend', () => {
+    img.style.animationName = '';
+  });
+  img.style.animationName = 'attackToRight';
+  img.style.animationDuration = '0.5s';
+};
+
 export default () => (
   <div class="buttons">
-    <button id="b1">Special Stomp</button>
+    <button onclick={attack} id="b1">
+      Special Stomp
+    </button>
     <button id="b2">Thunder Kick</button>
     <button id="b3">Crush</button>
     <button id="b4">Lick</button>
