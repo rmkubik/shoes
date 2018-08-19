26193e063bdab48b2510d1d311095260fba1df02
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-08-19 10:51:46 -0700

    Switch back to map scene after defeating enemy

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,17 @@ import { h } from 'hyperapp';
 import BattleScene from './BattleScene';
 import MapScene from './MapScene';
 
+const isEnemyDead = enemy => enemy.hp.current <= 0;
+
 const sceneMap = {
   BattleScene: (state, actions) => <BattleScene state={state} actions={actions} />,
   MapScene: (state, actions) => <MapScene state={state} actions={actions} />,
 };
 
-export default (state, actions) => (
-  <div class="main">{sceneMap[state.scene.current](state, actions)}</div>
-);
+export default (state, actions) => {
+  if (isEnemyDead(state.enemy) && state.scene.current === 'BattleScene') {
+    actions.changeScene({ newScene: 'MapScene' });
+  }
+
+  return <div class="main">{sceneMap[state.scene.current](state, actions)}</div>;
+};
