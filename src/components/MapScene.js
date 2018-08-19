704e8d273def64e18b14d204f52c2fd173a39671
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-08-19 10:18:05 -0700

    Create scene objects

---
+++ src/components/MapScene.js
@@ -0,0 +1,18 @@
+import { h } from 'hyperapp';
+
+export default ({ state, actions }) => (
+  <div class="mapScene">
+    <h3> Route 5 </h3>
+    <ol>
+      <li> Wild Shoe! </li>
+      <li> Wild Shoe! </li>
+      <li> Trainer Battle </li>
+      <li> Shop </li>
+      <li> Wild Shoe! </li>
+      <li> Wild Shoe! </li>
+      <li> Trainer Battle </li>
+      <li> Shop </li>
+      <li> Final Four </li>
+    </ol>
+  </div>
+);
