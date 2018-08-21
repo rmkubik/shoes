6ae09b7b4eb35a051fdfa1f9db3e56e6b820a06e
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-08-20 22:23:13 -0700

    Create items state

---
+++ src/state/items.js
@@ -0,0 +1,14 @@
+export default {
+  shoeBox: {
+    name: 'Shoe Box',
+    cost: 10,
+  },
+  elasticLaces: {
+    name: 'Elastic Laces',
+    cost: 20,
+  },
+  velcroStrap: {
+    name: 'Velcro Strap',
+    cost: 20,
+  },
+};
