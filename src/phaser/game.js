5bc7c6b8ef4f1b6b472e395a5d13b96e138c05e7
Ryan Kubik <rmkubik@me.com>
2018-10-01 17:52:14 -0700

    Add collisions into movement

--- src/phaser/game.js
+++ src/phaser/game.js
@@ -32,6 +32,21 @@ function create() {
   const sceneryLowerLayer = map.createStaticLayer('sceneryLower', tiles, 0, 0);
   const objectsLayer = map.createStaticLayer('objects', tiles, 0, 0);
 
+  objectsLayer.setCollisionBetween(0, 1765); // number of tiles in tiles spritesheet, TODO: dynamically calculate
+
+  console.log(map.layers.find(layer => layer.name === 'objects').data);
+
+  this.objects = this.add.group({
+    runChildUpdate: true,
+  });
+
+  this.wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+  this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+  this.sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+  this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+  const speed = 200;
+
   const prefab = new Prefab({
     scene: this,
     position: {
@@ -42,12 +57,47 @@ function create() {
     sprite: 0,
   });
 
-  const speed = 16;
+  prefab.speed = speed;
 
-  this.input.keyboard.on('keydown_W', () => { prefab.y -= speed; });
-  this.input.keyboard.on('keydown_A', () => { prefab.x -= speed; });
-  this.input.keyboard.on('keydown_S', () => { prefab.y += speed; });
-  this.input.keyboard.on('keydown_D', () => { prefab.x += speed; });
+  prefab.update = () => {
+    const direction = { x: 0, y: 0 };
+    prefab.body.setVelocity(0);
+
+    if (this.wKey.isDown) {
+      direction.y = -1;
+    }
+    if (this.aKey.isDown) {
+      direction.x = -1;
+    }
+    if (this.sKey.isDown) {
+      direction.y = 1;
+    }
+    if (this.dKey.isDown) {
+      direction.x = 1;
+    }
+
+    if (direction.x !== 0 || direction.y !== 0) {
+      prefab.direction = direction;
+
+      prefab.body.setVelocityX(prefab.speed * prefab.direction.x);
+      prefab.body.setVelocityY(prefab.speed * prefab.direction.y);
+
+      // console.log(prefab.direction);
+      // console.log(prefab.body.velocity);
+      // console.log(prefab.speed);
+    }
+  };
+
+  this.physics.add.collider(prefab, objectsLayer);
+  prefab.body.setCollideWorldBounds(true);
+
+  this.objects.add(prefab);
+
+
+  // this.input.keyboard.on('keydown_W', () => { prefab.y -= speed; });
+  // this.input.keyboard.on('keydown_A', () => { prefab.x -= speed; });
+  // this.input.keyboard.on('keydown_S', () => { prefab.y += speed; });
+  // this.input.keyboard.on('keydown_D', () => { prefab.x += speed; });
 }
 
 function update() {}
