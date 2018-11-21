9f8869e1bb5c15d74ad9e5be2f2304ef8eb090e3
Ryan Kubik <rmkubik@me.com>
2018-11-20 21:39:31 -0800

    First pass pop over info text on buttons

--- src/phaser/prefabs/button.js
+++ src/phaser/prefabs/button.js
@@ -10,7 +10,17 @@ class Button extends Prefab {
       scene, position, sheet, sprite: sprites.up,
     });
 
+    this.position = position;
+    this.scene = scene;
+
     this.text = scene.add.text(position.x + 5, position.y + 5, text, {
+      fontFamily: 'Arial',
+      fontSize: 16,
+      color: '#FFFFFF',
+    });
+
+    this.graphics = scene.add.graphics();
+    this.popOverText = scene.add.text(position.x, position.y, '', {
       fontFamily: 'Arial',
       fontSize: 16,
       color: '#FFFFFF',
@@ -26,7 +36,13 @@ class Button extends Prefab {
         { name: 'up', from: 'down', to: 'hover' },
       ],
       methods: {
-        onEnterHover: () => { this.setFrame(sprites.hover); },
+        onEnterHover: () => {
+          this.setFrame(sprites.hover);
+          this.showPopOver();
+        },
+        onLeaveHover: () => {
+          this.hidePopOver();
+        },
         onEnterUp: () => { this.setFrame(sprites.up); },
         onEnterDown: () => { this.setFrame(sprites.down); },
         onAfterUp: () => { onclick(); },
@@ -52,7 +68,22 @@ class Button extends Prefab {
     });
   }
 
+  showPopOver() {
+    this.graphics.fillStyle(0x000000);
+    this.graphics.fillRect(this.position.x, this.position.y, 160, 80);
+    this.popOverText.setText('more info');
+    this.scene.children.bringToTop(this.graphics);
+    this.scene.children.bringToTop(this.popOverText);
+  }
+
+  hidePopOver() {
+    this.graphics.clear();
+    this.popOverText.setText('');
+  }
+
   destroy() {
+    this.graphics.destroy();
+    this.popOverText.destroy();
     this.text.destroy();
     super.destroy();
   }
